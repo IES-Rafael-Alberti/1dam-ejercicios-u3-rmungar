f04bd6637181435dfa985e360ceca25d3f6128a9
rmungar1209 <rmungar1209@g.educaand.es>
2023-11-23 01:43:30 +0100

    ñ

--- src/Ejercicio3_2_11(terminar).py
+++ src/Ejercicio3_2_11(terminar).py
@@ -2,10 +2,8 @@ from BORRARCONSOLA import borrarconsola
 
 
 def separador(lineas: str):
-    linea = lineas.split("\n")
-    nom_campos = linea[0]
     datos = lineas.split(";")
-    clasificador(nom_campos, datos)
+    clasificador(datos)
         
         
 def clasificador(nom_campos, datos):
@@ -17,11 +15,7 @@ def clasificador(nom_campos, datos):
 
 def main():
     borrarconsola()
-    lineas = """nif;nombre;email;teléfono;descuento\n
-    01234567L;Luis González;luisgonzalez@mail.com;656343576;12.5\n
-    71476342J;Macarena Ramírez;macarena@mail.com;692839321;8\n
-    63823376M;Juan José Martínez;juanjo@mail.com;664888233;5.2\n
-    98376547F;Carmen Sánchez;carmen@mail.com;667677855;15.7"""
+    lineas = [("01234567L;Luis González;luisgonzalez@mail.com;656343576;12.5"),("71476342J;Macarena Ramírez;macarena@mail.com;692839321;8"), ("63823376M;Juan José Martínez;juanjo@mail.com;664888233;5.2"),("98376547F;Carmen Sánchez;carmen@mail.com;667677855;15.7")]
     
     separador(lineas)
     
